74717866d05ed0de82b544d3e6e139b2d9780218
blckcode <20210713m.princepaquiado.bscs@gmail.com>
2023-12-22 03:53:46 +0800

    initial commit

--- index.php
+++ index.php
@@ -2,91 +2,76 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Coffee House</title>
+    <title>Coffee Shop</title>
     <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@400;500&family=Poppins:wght@700&display=swap');
-        body {
-            background-image: url('https://popmenucloud.com/cdn-cgi/image/width=640,height=640,format=png,fit=scale-down/humndajp/d63eabd5-6815-44dd-b250-e80e8c14376f.jpg');
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
-            min-height: 100vh;
-        }
-        .navbar.bg-dark {
-            background-color: transparent !important;
-        }
-        
-        footer {
-            background-color: #f0f0f0;
-            color: white; /* Change to your preferred text color */
-            padding: 20px 0;
-            width: 100%;
-            position: fixed;
-            bottom: 0;
-            text-align: center;
-        }
-        footer a {
-            color: white; /* Change to your preferred link color */
-            text-decoration: none;
-        }
-    </style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" >
+  
 </head>
 <body>
-    
-    <nav class="navbar navbar-dark bg-dark fixed-top">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Prince's Cafe
-        <!-- <img class="logo" src="" alt="Logo" width="50" height="44" class="d-inline-block align-text-top"> -->
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Coffee House</h5>
-            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Menu</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Others
-                </a>
-                <ul class="dropdown-menu dropdown-menu-dark">
-                <li><a class="dropdown-item" href="#">About Us</a></li>
-                <li><a class="dropdown-item" href="#">Contact</a></li>
-                <li>
-                    <hr class="dropdown-divider">
-                </li>
-                <li><a class="dropdown-item" href="#">Location</a></li>
-                </ul>
-            </li>
-            </ul>
-        </div>
-        </div>
-    </div>
+  <header class="header">
+    <a href="#" class="logo">
+      <img src="assets/logo.png" alt="" >
+    </a>
+
+    <nav class="navbar">
+      <a href="#home">home</a>
+      <a href="#about">About</a>
+      <a href="#menu">Menu</a>
+      <a href="#review">Review</a>
+      <a href="#product">Product</a>
+      <a href="#contact">Contact</a>
+      <a href="#blogs">Blogs</a>
     </nav>
 
-    <footer class="bg-dark fixed-bottom">
+    <div class="icons">
+      <div class="fas fa-search" id="search-btn"></div>
+      <div class="fas fa-shopping-cart" id="cart-btn"></div>
+      <div class="fas fa-bars" id="cart-btn"></div>
+    </div>
 
+    <div class="search-form">
+      <input type="search" id="search-box" placeholder="search here...">
+      <label for="search-box" class="fas fa-search"></label>
+    </div>
 
-  <!-- Copyright -->
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
-    © 2023 Copyright:
-    <a class="text-white" href="https://github.com/blckcode">blckcode</a>
-  </div>
-  <!-- Copyright -->
-</footer>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <div class="cart-items-container">
+        <div class="cart-item">
+          <span class="fas fa-times"></span>
+          <img src="assets/logo.png" alt="">
+          <div class="content">
+            <h3>cart item 01</h3>
+            <div class="price">₱249.99/-</div>
+          </div>
+        </div>
+        <div class="cart-item">
+          <span class="fas fa-times"></span>
+          <img src="assets/logo.png" alt="">
+          <div class="content">
+            <h3>cart item 01</h3>
+            <div class="price">₱249.99/-</div>
+          </div>
+        </div>
+        <div class="cart-item">
+          <span class="fas fa-times"></span>
+          <img src="assets/logo.png" alt="">
+          <div class="content">
+            <h3>cart item 01</h3>
+            <div class="price">₱249.99/-</div>
+          </div>
+        </div>
+        <div class="cart-item">
+          <span class="fas fa-times"></span>
+          <img src="assets/logo.png" alt="">
+          <div class="content">
+            <h3>cart item 01</h3>
+            <div class="price">₱249.99/-</div>
+          </div>
+        </div>
+        <a href="#" class="btn" >check out now</a>
+    </div>
+  </header>
+  <script src="script.js"></script>
 </body>
 </html>
